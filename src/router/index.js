cbf8779a55bd1df1922ba8a23083bf2da5e0574f
Antoxerrr <a.malyarov99@gmail.com>
2021-01-04 04:03:24 +1000

    travis

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? '/stms_client/' : '/',
   routes,
 });
 
